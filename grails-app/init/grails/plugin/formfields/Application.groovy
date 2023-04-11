3160bfb210847b448b1c3af2b8ed57ad8d49f43f
Michael Yan <rain@rainboyan.com>
2023-04-11 12:41:17 +0800

    Update Dependencies
    - Grails 2022.0.1
    
    - GSP 2022.0.1
    
    - Scaffolding 5.1.1

--- grails-app/init/grails/plugin/formfields/Application.groovy
+++ grails-app/init/grails/plugin/formfields/Application.groovy
@@ -1,15 +1,14 @@
 package grails.plugin.formfields
 
-import grails.boot.GrailsApp
-import grails.boot.config.GrailsAutoConfiguration
+import grails.boot.Grails
 import grails.plugins.metadata.PluginSource
 import groovy.transform.CompileStatic
 
 @CompileStatic
 @PluginSource
-class Application extends GrailsAutoConfiguration  {
+class Application {
 
     static void main(String[] args) {
-        GrailsApp.run(Application, args)
+        Grails.run(Application, args)
     }
 }
